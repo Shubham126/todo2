74680139c5c5283c8970460cc7cf9ae6796a9228
Subham <chromaticshubham26@gmail.com>
2024-01-25 07:20:10 +0530

    added db.js

---
+++ backend/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+// Todo{
+//     title: string,
+//     description: string,
+//     completed: boolean,
+// }
+
+// mongo url handle
+mongoose.connect("mongodb+srv://chromaticshubham26:shubham26@cluster0.ufzfhxj.mongodb.net/")
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean,
+})
+
+const todo = mogoose.model('todos', todoSchema)
+
+module.exports = {
+    todo
+}
