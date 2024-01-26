43ba55544579142f7b12bba20d33cdf39ef4446a
Subham <chromaticshubham26@gmail.com>
2024-01-26 11:28:37 +0530

    CreateTodo using document.getElementById

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -2,12 +2,15 @@
 
 const CreateTodo = () => {
   return (
+    // react query 
+    // this will help us get rid from document.getElementById 
     <div>
-      <input style = {{
+      <input id = "title" style = {{
         padding: 10,
         margin: 10,
       }} type = "text" placeholder='title'></input> <br />
-      <input style = {{
+
+      <input id = "desc" style = {{
         padding: 10,
         margin: 10,
       }} type = "text" placeholder = "description"></input> <br />
@@ -15,6 +18,18 @@ const CreateTodo = () => {
       <button style = {{
         padding: 10,
         margin: 10,
+      }} onClick = {() => {
+        fetch("http://localhost:3000/todos", {
+            method: "POST",
+            body: {
+                title: document.getElementById("title").innerHTML,
+                description: document.getElementById("desc").innerHTML
+            }
+        }
+        .then(async function(res){
+            const json = await res.json();
+            alert("Todo Added");
+        }))
       }}> Add a todo </button>
     </div>
   )
